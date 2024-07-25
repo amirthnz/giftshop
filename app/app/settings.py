f9d5701278df4cdf61a1744b825791f47bc5eabc
amirthnz <amirthn95@gmail.com>
2024-07-25 00:28:03 -0700

    Added API documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+
     # Local apps
     'core',
 ]
